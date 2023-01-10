6a62f832c5ad8f1f149f5f5dfd602ecfe6822e38
Raeme Khatib <raeme.khatib@telus.com>
2023-01-10 14:13:10 -0700

    impliment tests for daylistitem and format spot function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,15 +5,30 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
   const dayClass = classNames(
-    "day-list__item",
-     { "day-list__item--selected": props.selected,
-      "day-list__item--full": !props.spots }
-  );
+    'day-list__item',
+    {'day-list__item--selected': props.selected,
+      'day-list__item--full': !props.spots}
+
+  )
+
+  const formatSpots = (input) => {
+    let output = `${input} spots remaining`
+    if (input === 1) {
+      output = `${input} spot remaining`
+    }
+    if (input === 0) {
+      output = `no spots remaining`
+    }
+    return output;
+  }
+
+  const spotsText = formatSpots(props.spots)
 
   return (
     <li onClick={() => props.setDay(props.name)}>
       <h2 className={dayClass}>{props.name}</h2>
-      <h3 className={dayClass}>{props.spots} spots remaining</h3>
+      <h3 className={dayClass}>{spotsText}</h3>
     </li>
   );
 }
+
