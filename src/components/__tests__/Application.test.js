bd0643f3b16e7a58c3011ebf9c743607351560a9
Raeme Khatib <raeme.khatib@telus.com>
2023-01-17 21:49:55 -0700

    edit the interview test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -56,26 +56,39 @@ describe("Appointment", () => {
 
   });
 
-  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async() => {
+    //  1. Render the Application.
     const { container } = render(<Application />);
+  
+    // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
     const appointment = getAllByTestId(container, "appointment").find(
       appointment => queryByText(appointment, "Archie Cohen")
     );
-    fireEvent.click(getByAltText(appointment, "Edit"));
-    fireEvent.change(getByDisplayValue(appointment, "Archie Cohen"), {
+  
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+  
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
     fireEvent.click(getByText(appointment, "Save"));
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
-    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
-    expect(getByText(appointment, "Lydia Miller-Jones")).toBeInTheDocument();
+  
+    await waitForElement(() => getByText(container, "Sylvia Palmer"))
+    expect(getByText(container, "Sylvia Palmer")).toBeInTheDocument();
+    const day = getAllByTestId(container, "day").find(day => 
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "1 spot remaining"))
+  
   });
 
   it("shows the save error when failing to save an appointment", () => {
     axios.put.mockRejectedValueOnce();
   });
-  
+
   it("shows the delete error when failing to delete an existing appointment", () => {
     axios.delete.mockRejectedValueOnce();
   });
